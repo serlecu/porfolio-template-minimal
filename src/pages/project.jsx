64ac943a2ebd364e01057fdb73e3e1c3a2a1b296
Sergio Lecuona <serlecu@hotmail.com>
2023-02-06 15:38:34 +0100

    Clean project
    - Remove comments
    - Remove console log

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -8,35 +8,27 @@ import pagesData from '../data';
 const Project = ({ projectId }) => {
 
   const project = pagesData.find((element) => element.id === projectId);
-  console.log(project);
 
   return (
     <section className='container text-left px-0 overflow-hidden'>
 
-      {/* Title & Details */}
       <div className='proj-header d-flex row gx-2 mb-4 align-items-center'>
         <ProjTitle title={project.title} />
         <ProjDetails details={project.details} />
       </div>
 
-      {/* Content */}
       <div className='proj-content row row-cols-2 g-4'>
-
-        {/* <div className='col-5 px-0'> */}
-        {/* Featured */}
         {project.featured ?
           (
             <div className='proj-featured'>
               <ProjectMediaItem item={project.featured} />
             </div>
           ) : null}
-        {/* Descriptions */}
+
         <div className='proj-desc'>
           <ProjDescription descriptions={project.description} />
         </div>
-        {/* Media */}
-        {/* <div className='proj-media col'> */}
-        {/* </div> */}
+
         {project.media.map((item, index) =>
         (
           <div className={`proj-media-${item.type}`}
@@ -45,21 +37,6 @@ const Project = ({ projectId }) => {
           </div>
         ))
         }
-
-        {/* </div> */}
-
-        {/* <div className='col-6'> */}
-        {/* Descriptions */}
-        {/* <div className='row p-0 py-2 gy-5'>
-            {project.description.map((text, index) => (
-              <p key={index}
-                className='proj-description col-12'>
-                {text.value}
-              </p>
-            ))}
-          </div>
-        </div> */}
-
       </div>
 
     </section>
