d71b6624c5d852cc63382d07722d0488fc5de9ac
Sergio Lecuona <serlecu@hotmail.com>
2023-02-06 14:21:44 +0100

    Fix ProjectDesription margin between languages

--- src/components/pure/projDescription.jsx
+++ src/components/pure/projDescription.jsx
@@ -4,17 +4,31 @@ import PropTypes from 'prop-types';
 
 const ProjDescription = ({ descriptions }) => {
   return (
-    <div>
-      {descriptions.map((text, index) => {
-        const className = `proj-description-${text.language} ${text.language === 'es' ? 'text-secondary' : ''}`;
-        return(
-        <p key={index}
-          className={className}
-        >
-          {text.value}
-        </p>
-        )
-      })}
+    <div className='container p-0 g-10'>
+    <div className='proj-description-en mb-4'>
+      {
+        descriptions.filter(({ language }) => language === 'en')
+        .map((text, index) => {
+            return (
+              <p key={index}>
+                {text.value}
+              </p>
+            )
+          })
+      }
+    </div>
+    <div className='proj-description-es text-secondary'>
+      {
+        descriptions.filter(({ language }) => language === 'es')
+          .map((text, index) => {
+            return (
+              <p key={index}>
+                {text.value}
+              </p>
+            )
+          })
+      }
+    </div>
     </div>
   );
 };
