6c7f8db7d52b5539621f59fbc55de91fa65daa52
Sergio Lecuona <serlecu@hotmail.com>
2023-02-06 13:28:45 +0100

    Home image swhitch loop

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Home() {
+
+  const [currentPicIndex, setCurrentPicIndex] = useState(0);
+
+  const images = [
+    {
+      src: '/images/1-Captura-de-pantalla-2016-02-18-a-las-21-38-40EXP2.jpg',
+      alt: 'Sulveilance camera still of deers in the forest at night spotted with a strong light.',
+    },
+    {
+      src: '/images/9-Captura-de-pantalla-2017-04-05-a-las-13-07-59EXP.jpg',
+      alt: 'Sulveilance camera still of a worksite from a top-down view in the middle of the city at daylight.',
+    },
+    {
+      src: '/images/2-Captura-de-pantalla-2017-04-05-a-las-13-40-45EXP2.jpg',
+      alt: 'Sulveilance camera still of a worksite from a top-down view in the middle of the city at daylight.',
+    },
+  ];
+
+  useEffect(() => {
+    changeImageRandom();
+  }, []);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeImageNext();
+    }, 6000);
+    return () => clearInterval(interval);
+  }, [currentPicIndex]);
+
+  function changeImageNext() {
+    const nextPicIndex = (currentPicIndex + 1) % (images.length);
+    setCurrentPicIndex(nextPicIndex);
+  }
+  function changeImageRandom() {
+    const picRandom = Math.floor(Math.random() * (images.length - 1));
+    setCurrentPicIndex(picRandom);
+  }
+
   return (
     <section className='container text-left px-0 overflow-hidden'>
       <div className='home-content row row-cols-1 g-4'>
-      <img className='proj-media-img w-10 img-fluid'
-          src="/images/1-Captura-de-pantalla-2016-02-18-a-las-21-38-40EXP2.jpg"
-          alt="Sulveilance camera still of deers in the forest at night spotted with a strong light."
-        />
+
         <img className='proj-media-img w-10 img-fluid'
-          src="/images/9-Captura-de-pantalla-2017-04-05-a-las-13-07-59EXP.jpg"
-          alt="Sulveilance camera still of a worksite from a top-down view in the middle of the city at daylight."
+          src={images[currentPicIndex].src}
+          alt={images[currentPicIndex].alt}
         />
-        <img className='proj-media-img w-10 img-fluid'
-          src="/images/2-Captura-de-pantalla-2017-04-05-a-las-13-40-45EXP2.jpg"
-          alt="Sulveilance camera still of an empty parking from a top-down. Only diagonal yelow lines over the asphalt are visible."
-        />
-      <div className='home-text'>
-            <p className='home-introduction'>
-            </p>
+
+        <div className='home-text'>
+          <p className='home-introduction'>
+          </p>
         </div>
 
       </div>
